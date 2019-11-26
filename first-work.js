579da8a13675937bfcb66708f91ad3112a497c81
alisher <shera.rashiduulu@gmail.com>
2019-11-26 16:20:45 +0200

    First work has done

---
+++ first-work.js
@@ -0,0 +1,18 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var toAdd = document.createDocumentFragment();
+function IntervalCreateDiv() {
+   var newDiv = document.createElement('div');
+   newDiv.innerText = getRandomIntInclusive(0, 20);
+   newDiv.className = 'element';
+   toAdd.appendChild(newDiv);
+   console.log(newDiv);
+   var parentDiv = document.getElementById('container');
+   parentDiv.appendChild(toAdd);
+}
+setInterval(IntervalCreateDiv, 3000);
